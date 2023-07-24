e5cd89b574bcbe58ddf49412cdce217a093488e9
jhwii <jhwii98@naver.com>
2023-07-24 16:11:35 +0900

    0724

--- backend/intellij/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ backend/intellij/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,21 +2,19 @@ package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @ToString
-@Getter
-@Setter
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
+    // update 를 위한 dto 변경 - id 필드 추가 및 엔티티 반환 메소드 변경
 
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
     // toEntity() 메소드는 새로운 Article 을 생성하는데 기본인 id는 null 로 설정
     // title 과 content 는 dto 에서 전달된 값으로 설정
