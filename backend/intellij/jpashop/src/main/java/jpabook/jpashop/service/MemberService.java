0628453deb26cec12d2fcd9697593f4e0cf1ddc0
jhwii <jhwii98@naver.com>
2023-08-02 17:47:18 +0900

    0802

--- backend/intellij/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ backend/intellij/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -27,8 +27,8 @@ public class MemberService {
         if(!findMembers.isEmpty()){
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
-
     }
+
     // 회원 전체 조회
     public List<Member> findMembers(){
         return memberRepository.findAll();
