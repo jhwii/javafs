66251e90165f78cccf9e15dc58c75a791e7ef6a4
jhwii <jhwii98@naver.com>
2023-08-29 18:22:54 +0900

    0829

--- backend/intellij2/board_crud/src/main/java/spring_study/board_crud/service/BoardService.java
+++ backend/intellij2/board_crud/src/main/java/spring_study/board_crud/service/BoardService.java
@@ -21,4 +21,24 @@ public class BoardService {
     public Board findOne(Long id){
         return boardRepository.findById(id).orElseThrow(NullPointerException::new);
     }
+
+    @Transactional
+    public void create(Board board) {
+        boardRepository.save(board);
+    }
+
+    @Transactional
+    // Dirty Checking 으로 update 수행
+    public void update(Long id, String title, String content) {
+        Board board = boardRepository.findById(id).orElseThrow(NullPointerException::new);
+        board.setTitle(title);
+        board.setContent(content);
+    }
+    // id에 해당 하는 board 가 repository 에 존재 하지 않을 경우 NullPointerException 에러 핸들링
+    // (** 서버 죽지 않게 하기 위함 **)
+
+    public void delete(Board board){
+        boardRepository.delete(board);
+    }
+
 }
