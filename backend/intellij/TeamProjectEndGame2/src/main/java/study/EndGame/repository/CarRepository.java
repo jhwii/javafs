c254b7f7b95a8e22a274581b8533164a2d98aae8
jhwii <jhwii98@naver.com>
2023-09-15 15:21:36 +0900

    0915

--- backend/intellij/TeamProjectEndGame2/src/main/java/study/EndGame/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package study.EndGame.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import study.EndGame.entity.Car;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
